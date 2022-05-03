8d1a323bde17aa97ab0bab91eac96bed21dab43f
alexhvoyer <alexhvoyer@gmail.com>
2022-05-03 12:03:34 +0400

    Added titan model

--- library/admin.py
+++ library/admin.py
@@ -2,5 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Character
+from .models import Titan
 
 admin.site.register(Character)
+admin.site.register(Titan)
--- library/models.py
+++ library/models.py
@@ -1,4 +1,27 @@
 from django.db import models
+from uuid import uuid4
+
+class Titan(models.Model):
+    ELDIA = 'eldia'
+    MARLEY = 'marley'
+
+    ALLEGIANCE_CHOISES = [
+        (ELDIA, 'Eldia'),
+        (MARLEY, 'Marley')
+    ];
+
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    name = models.CharField(max_length=200)
+    other_names = models.CharField(max_length=200, null=True)
+    abilities = models.CharField(max_length=200)
+    allegiance = models.CharField(
+        choices=ALLEGIANCE_CHOISES,
+        default=MARLEY,
+        max_length=10
+    )
+
+    def __str__(self) -> str:
+        return self.name;
 
 # Create your models here.
 class Character(models.Model):
@@ -12,7 +35,7 @@ class Character(models.Model):
         (UNKNOWN, 'Unknown')
     ]
 
-
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
     age = models.PositiveSmallIntegerField()
@@ -25,9 +48,10 @@ class Character(models.Model):
         max_length=20
     )
     alias = models.CharField(max_length=200, null=True)
+    titan = models.OneToOneField(to=Titan, null=True, on_delete=models.SET_NULL)
 
     def __str__(self) -> str:
-        return self.first_name
+        return "%s %s" % (self.first_name, self.last_name)
 
     def isTeenager (self) -> bool:
         return self.age < 20
